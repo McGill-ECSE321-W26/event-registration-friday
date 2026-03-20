6e8c6a9a41a8048d8107260e29f08d6c1966391e
mcgill-sfeng <shuzhao.feng@mail.mcgill.ca>
2026-03-20 14:24:53 -0400

    Add EventView

--- event-registration-frontend/src/App.vue
+++ event-registration-frontend/src/App.vue
@@ -1,11 +1,16 @@
-<script setup></script>
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/events">Events</RouterLink>
+    </nav>
+  </header>
+  <RouterView />
 </template>
 
 <style scoped></style>
